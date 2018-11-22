645ae8648097a7a09c02a9c83b44769965612896
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2018-11-22 18:35:30 +0100

    Displays a whole set of colourful triangles

--- main.cpp
+++ main.cpp
@@ -5,83 +5,29 @@
 
 constexpr GLdouble sceneHeight = 1000;
 constexpr GLdouble sceneWidth = 1000;
-constexpr GLdouble triangleSideWidth = sceneWidth / 5;
+constexpr GLdouble triangleSideWidth = sceneWidth / 6;
 const GLdouble triangleHeight = std::sqrt(
 	std::pow(triangleSideWidth, 2) - std::pow(triangleSideWidth / 2, 2));
 
+void init();
+void draw_scene();
+void draw_quater();
+void draw_triangle_tower(unsigned triangle_count);
+void set_random_color();
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
+	glutInitWindowSize(640, 480);//initial window size
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Spinning Triangles");//create widnow, hello title bar
+	init();
+	glutDisplayFunc(draw_scene);//
+	glutMainLoop();//enter main loop and process events
+	return 0;
+}
 //Chaninging the dierctions of rotation of rings and triangles should be possible
-
-void drawInnerRing()
-{
-
-}
-
-void MyDisplay(void) {
-	// The new scene
-	glLoadIdentity();
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-//	glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-	glBegin(GL_POLYGON);// OpenGL�a state
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	glVertex2f(100.0f, 50.0f);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-	glVertex2f(450.0f, 400.0f);
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue
-	glVertex2f(450.0f, 50.0f);
-	glEnd();
-	glPopMatrix();
-
-	glBegin(GL_POLYGON);
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue
-	glVertex2f(450.0f, 400.0f);
-	glVertex2f(800.0f, 50.0f);
-	glVertex2f(800.0f, 400.0f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex2f(800.0f, 50.0f);
-	glVertex2f(450.0f, 50.0f);
-	glVertex2f(450.0f, 400.0f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-	glVertex2f(100.0f, 50.0f);
-	glVertex2f(100.0f, 400.0f);
-	glVertex2f(450.0f, 400.0f);
-	glEnd();
-
-	glBegin(GL_TRIANGLES);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex2f(100.0f, 400.0f);
-	glVertex2f(100.0f, 750.0f);
-	glVertex2f(450.0f, 400.0f);
-
-	//glPushMatrix();
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-	glVertex2f(100.0f, 750.0f);
-	glVertex2f(450.0f, 750.0f);
-	glVertex2f(450.0f, 400.0f);
-	//glPopMatrix();
-
-	glVertex2f(450.0f, 750.0f);
-	glVertex2f(800.0f, 750.0f);
-	glVertex2f(800.0f, 400.0f);
-
-	glVertex2f(800.0f, 400.0f);
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-	glVertex2f(450.0f, 400.0f);
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-	glVertex2f(450.0f, 750.0f);
-
-	glEnd();
-
-	// The end of scene
-	glFlush();//start processing buffered OpenGL routines
-}
-void MyInit(void) {
+void init(void) {
 	glClearColor(0.0, 0.0, 0.0, 0.0);//select clearing (background) color
 									 /* initialize viewing values */
 	glViewport(0, 0, 500, 500);//window origin and size
@@ -92,14 +38,52 @@ void MyInit(void) {
 	glLoadIdentity();//=1
 }
 
-int main(int argc, char** argv) { //<- for normal API
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
-	glutInitWindowSize(640, 480);//initial window size
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Spinning Triangles");//create widnow, hello title bar
-	MyInit();
-	glutDisplayFunc(MyDisplay);//
-	glutMainLoop();//enter main loop and process events
-	return 0;
+void draw_scene(void) {
+	// The new scene
+	glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	for(unsigned side = 0; side < 4; ++side) {
+	glPushMatrix();
+	glTranslatef(triangleSideWidth * 3, triangleSideWidth * 3, 0.0f);
+	glRotatef(90 * side, 0.0f, 0.0f, 1.0f);
+	draw_quater();
+	glPopMatrix();
+	}
+	// The end of scene
+	glFlush();//start processing buffered OpenGL routines
+}
+void draw_quater()
+{
+	glPushMatrix();
+	for (unsigned i = 3; i > 0; --i) {
+	draw_triangle_tower(i);
+	glTranslatef(triangleSideWidth, 0.0f, 0.0f);
+	}
+	glPopMatrix();
+}
+
+void draw_triangle_tower(unsigned int triangle_cout)
+{
+	for(unsigned i = 0; i < triangle_cout; ++i) {
+		glPushMatrix();
+		set_random_color();
+		glTranslatef(0.0f, triangleSideWidth * i, 0.0f);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(0.0f, 0.0f);
+		glVertex2f(0.0f, triangleSideWidth);
+		glVertex2f(triangleSideWidth, 0.0f);
+		glEnd();
+		glPopMatrix();
+	}
+	glFlush();
+}
+
+void set_random_color()
+{
+	glColor4f(
+		std::rand() % 10 * 0.1f,
+		std::rand() % 10 * 0.1f,
+		std::rand() % 10 * 0.1f,
+		1.0f);
 }
